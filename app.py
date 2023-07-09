e04e3b9a7e1def979ede7603d8df18c0a16180ed
elliSzAt <125866921+elliSzAt@users.noreply.github.com>
2023-07-09 21:05:40 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    template = """
+    <html>
+        <body>
+            <h1>Welcome to Trongdeptrai's website!</h1>
+            <p>{{ message }}</p>
+        </body>
+    </html>
+    """
+    message = request.args.get('message', '')
+    return render_template_string(template, message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
